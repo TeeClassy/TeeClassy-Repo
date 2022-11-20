0fa63444b6b8d7961d8f6faa06093c185a39c2d3
TeeClassy <omoiyasunnahnoni@gmail.com>
2022-11-20 13:22:11 +0100

    Money is good

---
+++ double.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+	float radius;
+	printf("please enter your radius");
+	scanf("%f", &radius);
+
+	printf("The radius value is %f\n", radius);
+	return (0);
+}
---
+++ tee.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int money;
+	printf("Please enter the amount you need:");
+	scanf("%i", &money);
+
+	printf("The amount i need is %i\n", money);
+	return (0);
+}
